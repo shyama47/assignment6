819dc2f142f9f0b731fe137f6353c3c031851b5c
shyama47 <shyamasaha588@gmail.com>
2025-05-11 12:16:32 +0600

    added an update code

--- script/input.js
+++ script/input.js
@@ -4,3 +4,22 @@ document.getElementById("faq-btn").addEventListener('click',function(){
 document.getElementById("learn-btn").addEventListener('click',function(){
     document.getElementById('learn-section').scrollIntoView({behavior:'smooth'})
 })
+
+
+document.getElementById('click-btn').addEventListener('click',function(){
+    const inputText=document.getElementById('Text');
+    const inputPin=document.getElementById('Pin')
+    const text=inputText.value;
+    const pin=inputPin.value;
+    if(text!==''){
+      if(pin=='123456'){
+document.getElementById('faq-section').scrollIntoView({behavior:'smooth'})
+      }
+      else{
+        alert('pin tikh nai')
+      }
+    }
+    else{
+        alert('plz enter your name')
+    }
+})
